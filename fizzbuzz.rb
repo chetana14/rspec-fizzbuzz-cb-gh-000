911fde4f40a92088acb5a0d5d818d6311aacfbc1
Chetana Singhal <singhal.ct.1998@gmail.com>
2019-06-20 12:40:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
       "Fizz" # Go fizz
   if int % 5 == 0 # if the number int is divisible by 3
         "Buzz" # Go fizz
-  if int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   else
       nil
